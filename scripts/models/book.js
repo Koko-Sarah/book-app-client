f933f35a9591f371a93c9eeb418f6b9ea639db0b
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-19 13:02:05 -0800

    added comments to book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,2 +1,39 @@
 'use strict'
-const __API_URL__ = 'https://sb-kk-booklist.herokuapp.com/';
+
+var app = app || {};
+
+
+(function (module) {
+//this is the book constructor take key data from table rows create an instance of the book object 
+
+  function Book(bookDataObj) {
+    Object.keys(bookDataObj).forEach(key => this[key] = bookDataObj[key]);
+  }
+
+  Book.all = [];
+
+  Book.prototype.toHtml = function () {
+    var template = Handlebars.compile($('#book-template').text());
+    //not complete
+  };
+
+  //get data from server and push to constructor
+  Book.loadAll = rawData => {
+    Book.all = rawData.map(row=> new Book(row));
+  };
+
+//this will call the loadall function, taking the data from the server and pushing it into the new Book constructor
+
+  Book.fetchAll = callback => {
+    $.get('/books_app')
+      .then(results => {
+        Book.loadAll(results);
+        callback();
+      });
+  };
+
+
+
+
+
+})(app);
