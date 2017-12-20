b0da2128ccbeba7a7dc308c12b12f2b79b02b76f
Kowser Kassa <kowserk7@gmail.com>
2017-12-19 17:31:38 -0800

    followed along

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,2 +1,26 @@
-'use strict'
-const __API_URL__ = 'https://sb-kk-booklist.herokuapp.com/';
+'use strict';
+
+var app = app || {};
+(function (module) {
+  var __API_URL__ = 'https://sb-kk-booklist.herokuapp.com/';
+  function errorCallback(err){
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+  function Book(rawBookObj) {
+    Object.keys(rawBookObj).map(key => this[key] = rawBookObj[key]);
+  }
+  Book.prototype.toHtml = function() {
+    return Handlebars.compile($('#book-list-template').text())(this);
+  };
+
+  Book.all = [];
+  Book.loadAll = rows => Book.all = rows.sort((a,b) => a.title - b.title).map(book => new Book(book));
+  Book.fetchAll = callback =>{
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+  };
+  module.Book = Book;
+})(app);
