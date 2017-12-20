526e3f126bb5216dfdfb23e12f2a0142f2a25338
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-19 17:45:07 -0800

    fixed typo

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var app = app || {};
@@ -24,44 +23,5 @@ var app = app || {};
       .catch(errorCallback);
   };
   module.Book = Book;
-=======
-'use strict'
 
-var app = app || {};
-
-
-(function (module) {
-//this is the book constructor take key data from table rows create an instance of the book object 
-
-  function Book(bookDataObj) {
-    Object.keys(bookDataObj).forEach(key => this[key] = bookDataObj[key]);
-  }
-
-  Book.all = [];
-
-  Book.prototype.toHtml = function () {
-    var template = Handlebars.compile($('#book-template').text());
-    //not complete
-  };
-
-  //get data from server and push to constructor
-  Book.loadAll = rawData => {
-    Book.all = rawData.map(row=> new Book(row));
-  };
-
-//this will call the loadall function, taking the data from the server and pushing it into the new Book constructor
-
-  Book.fetchAll = callback => {
-    $.get('/books_app')
-      .then(results => {
-        Book.loadAll(results);
-        callback();
-      });
-  };
-
-
-
-
-
->>>>>>> f933f35a9591f371a93c9eeb418f6b9ea639db0b
 })(app);
