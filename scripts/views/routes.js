3087b00ce2640e3fd4eafa543a580d366868fa89
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-21 10:54:58 -0800

    implimented routes for index and detail page, both working

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,24 +2,7 @@
 'use strict';
 
 page('/', app.Book.fetchAll(app.bookView.initIndexPage));
-//page('/about', ctx => app.Book.fetchAll(app.bookView.initIndexPage(ctx)));
 
-page('/books/:id', ctx=> {
-    
-  ( app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage));
-  console.log('ctx is wuh', ctx);
+page('/books/:id', ctx => app.Book.fetchOne(ctx.params.id, app.bookView.initBookDetails));
 
-});
-
-//page();
-
-
-// app.bookView.initBookDetails();
-
-
-//previously on book-view page
-// $((ctx=> app.Book.fetchAll(app.bookView.initIndexPage(ctx));
-
-//ctx => app.Book.fetchOne(app.bookView.initDetailPage(ctx))
-
-//page('/user/:id', load, show)
+page();
