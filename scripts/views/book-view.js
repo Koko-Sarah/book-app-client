d09f76e4bc6145b62f02f09496943c04e71822db
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-21 11:27:15 -0800

    integrated koko's form code

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -20,6 +20,54 @@ var app = app || {};
     module.Book.all.map(book => $('#details-section').append(book.detailHtml()));
   };
 
+
+
+
+  ///////////////////////////////////Form functions
+  bookView.initNewBookPage = () => {
+    $('#new-form').show();
+    $('.container').hide();
+    $('#new-form').on('change', 'input, textarea', bookView.create);
+    $('#new-form').on('submit', bookView.submit);
+  };
+  bookView.create = () => {
+    let book;
+    $('#new-book').empty();
+
+    book = new Book({
+      title: $('#title').val(),
+      author: $('#author').val(),
+      isbn: $('#isbn').val(),
+      image_url: $('#image_url').val(),
+      description: $('#description').val(),
+    });
+
+    $('#new-book').append(book.toHtml());
+
+    $('pre code').each(function (i, block) {
+      hljs.highlightBlock(block);
+    });
+    $('#export-field').show();
+  };
+  bookView.submit = event => {
+    
+    event.preventDefault();
+    let book = new Book({
+      title: $('#title').val(),
+      author: $('#author').val(),
+      isbn: $('#isbn').val(),
+      image_url: $('#image_url').val(),
+      description: $('#description').val(),
+    });
+
+    book.insertBook();
+  };
+
+
+
+
+
+
   module.bookView = bookView;
 })(app);
 
