7ba791ec3a49abe76248863eed5e1cd356e9c471
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-21 13:05:03 -0800

    form linter error resolved, form working, submit info not

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -27,37 +27,37 @@ var app = app || {};
   bookView.initNewBookPage = () => {
     $('.container').hide();
     $('.new-book').show();
-    //   $('#new-book').on('change', 'input, textarea', bookView.create);
-    // $('#new-book').on('submit', bookView.submit);
+   // $('#new-book').on('change', 'input, textarea', bookView.create);
+    $('#new-book').on('submit', bookView.submit);
   };
-  
-  // bookView.create = () => {
-  //   let book;
-  //   $('#new-book').empty();
 
-  //   book = new Book({
-  //     title: $('#title').val(),
-  //     author: $('#author').val(),
-  //     isbn: $('#isbn').val(),
-  //     image_url: $('#image_url').val(),
-  //     description: $('#description').val(),
-  //   });
+  bookView.create = () => {
+    let newBook;
+    $('#new-book').empty();
 
-   
+    newBook = new app.Book ({
+      title: $('#title').val(),
+      author: $('#author').val(),
+      isbn: $('#isbn').val(),
+      image_url: $('#image_url').val(),
+      description: $('#description').val()
+    });
+  };
 
-  // bookView.submit = event => {
 
-  //   event.preventDefault();
-  //   let book = new Book({
-  //     title: $('#title').val(),
-  //     author: $('#author').val(),
-  //     isbn: $('#isbn').val(),
-  //     image_url: $('#image_url').val(),
-  //     description: $('#description').val(),
-  //   });
+  bookView.submit = event => {
 
-  //   book.insertBook();
-  // };
+    event.preventDefault();
+    let book = new app.Book ({
+      title: $('#title').val(),
+      author: $('#author').val(),
+      isbn: $('#isbn').val(),
+      image_url: $('#image_url').val(),
+      description: $('#description').val(),
+    });
+
+    book.insertBook();
+  };
 
 
 
